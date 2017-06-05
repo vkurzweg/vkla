a696ced69e2f69e99dd256bd91910b76d98d62ea
Victoria Dunham <vkurzweg@gmail.com>
2017-06-04 20:33:57 -0700

    add postinstall script

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const public_path = express.static(__dirname + '/public');
 const index_path = __dirname + '/public/index.html';
 
 app.use(public_path);
-// app.get('*', function (request, response) {
-//   response.sendFile(index_path, function (error) {
-//     if (error) {
-//       console.log(error);
-//     }
-//   });
-// });
+app.get('*', function (request, response) {
+  response.sendFile(index_path, function (error) {
+    if (error) {
+      console.log(error);
+    }
+  });
+});
 
 app.get('*.js', function (req, res, next) {
   req.url = req.url + '.gz';
